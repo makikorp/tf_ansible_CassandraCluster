d898343ccfe5524d5a4da911771a8ba208fb815e
makikorp <36685794+makikorp@users.noreply.github.com>
2023-04-07 11:22:06 -0700

    Add files via upload

--- seeds_cassandra.py
+++ seeds_cassandra.py
@@ -9,9 +9,9 @@ line = ipfile.readline()
 while line:
     line = line.rstrip(",")  # strip trailing spaces and newline
     # process the line
-    #print(f'- seeds: "{line}"')
     newseeds = '- seeds: "{var}"'.format(var=line)
     print(f'{newseeds}')
+
     with open('cassandra.yaml', 'r') as file:
         filedata = file.read()
     filedata = filedata.replace('- seeds: "127.0.0.1:7000"', '{}'.format(newseeds))
